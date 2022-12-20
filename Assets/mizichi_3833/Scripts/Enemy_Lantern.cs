57e1a2aebf01da209e7cea4d21e8d850b2be5993
Mizichi <jbc3584@utexas.edu>
2022-12-19 23:10:57 -0600

    Change Folder Name

---
+++ Assets/mizichi_3833/Scripts/Enemy_Lantern.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MIZICHI
+{
+    public class Enemy_Lantern : MonoBehaviour
+    {
+
+        void Update()
+        {
+
+        }
+
+        //Managers.MinigamesManager.DeclareCurrentMinigameWon();
+        //Managers.MinigamesManager.DeclareCurrentMinigameLost();
+    }
+}
+
---
+++ Assets/mizichi_3833/Scripts/Player_Transform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MIZICHI
+{
+  public class Player_Transform : MonoBehaviour
+    {
+      Animator NoliteAnim;
+        private void Start()
+        {
+            NoliteAnim = GetComponent<Animator>();
+        }
+        void Update()
+       {
+        
+       }
+
+
+
+    }
+}
+

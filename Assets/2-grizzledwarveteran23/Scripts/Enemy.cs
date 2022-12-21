4ee9b9d01bcefb8f11c770303d7dfd1034bd554c
grizzledwarvet23 <72232563+grizzledwarvet23@users.noreply.github.com>
2022-12-21 10:38:45 -0600

    Game is finished!

--- Assets/2-grizzledwarveteran23/Scripts/Enemy.cs
+++ Assets/2-grizzledwarveteran23/Scripts/Enemy.cs
@@ -6,9 +6,19 @@ namespace grizzledwarveteran23 {
     public class Enemy : MonoBehaviour
     {
         public float speed;
+        public float lifeTime;
+
+        void Start() {
+            StartCoroutine(Die(lifeTime));
+        }
         
         void FixedUpdate() {
             transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        IEnumerator Die(float time) {
+            yield return new WaitForSeconds(time);
+            Destroy(gameObject);
         }
     }
 }
--- Assets/2-grizzledwarveteran23/Scripts/EnemySpawner.cs
+++ Assets/2-grizzledwarveteran23/Scripts/EnemySpawner.cs
@@ -7,20 +7,25 @@ namespace grizzledwarveteran23 {
     {
         //Enemies will randomly spawn at a predetermined set of points
         [SerializeField]
-        private GameObject enemyPrefab;
+        private GameObject[] enemyPrefabs;
         [SerializeField]
-        private Transform[] spawnPoints = new Transform[3];
+        private Transform[] spawnPoints = new Transform[6];
 
         [SerializeField]
         private float spawnRate;
 
         private bool isSpawning = false;
 
+        int counter = 0;
+
+        int prefabIndex = 0;
+
         // Update is called once per frame
         void Update()
         {
-            if(!isSpawning)
+            if(!isSpawning && counter < 13)
             {
+                counter++;
                 StartCoroutine(SpawnEnemies());
             }
         }
@@ -29,9 +34,19 @@ namespace grizzledwarveteran23 {
         {
             isSpawning = true;
             yield return new WaitForSeconds(spawnRate);
-            int spawnPointIndex = Random.Range(0, spawnPoints.Length);
-            Instantiate(enemyPrefab, spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
-            spawnRate = Mathf.Max(0.5f, spawnRate - 0.1f);
+            int choice = Random.Range(0, 10);
+            int spawnPointIndex;
+            if(choice <= 7) {
+                 spawnPointIndex = Random.Range(0, 3);
+            } else {
+                spawnPointIndex = Random.Range(0, spawnPoints.Length);
+            }
+            GameObject instance = Instantiate(enemyPrefabs[prefabIndex], spawnPoints[spawnPointIndex].position, spawnPoints[spawnPointIndex].rotation);
+            if(spawnPointIndex >= 3) {
+                instance.GetComponent<Enemy>().speed = 6;
+            }
+            spawnRate = Mathf.Max(0.55f, spawnRate - 0.1f);
+            prefabIndex = (prefabIndex + 1) % enemyPrefabs.Length;
             isSpawning = false;
         }
     }

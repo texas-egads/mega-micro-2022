1e25c125b5d6db48c493064fcdb3d2af9e7cfa11
Adhan Razzaque <adhan.razzaque@gmail.com>
2022-12-20 18:16:49 -0600

    Add sounds for bucket states

--- Assets/Solo-General Red#8888/CapybaraBucket.cs
+++ Assets/Solo-General Red#8888/CapybaraBucket.cs
@@ -9,21 +9,46 @@ namespace Solo_General_Red_8888
         public static event Action BucketFilled;
         public static event Action BucketOverfilled;
 
+        // Public Properties
+        public AudioClip transformNeutralClip;
+        public AudioClip transformSuccessClip;
+        public AudioClip transformFailedClip;
+        
         // Private properties
         [SerializeField] private int maxFillLevel = 3;
         private int _fillLevel;
+        private AudioSource _bucketAudioSource;
+
+        private void Start()
+        {
+            _bucketAudioSource = Managers.AudioManager.CreateAudioSource();
+        }
 
         public void FillBucket()
         {
             ++_fillLevel;
-            if (_fillLevel == maxFillLevel)
+            if (_fillLevel < maxFillLevel)
             {
+                _bucketAudioSource.clip = transformNeutralClip;
+                _bucketAudioSource.Play();
+            }
+            else if (_fillLevel == maxFillLevel)
+            {
+                _bucketAudioSource.clip = transformSuccessClip;
+                _bucketAudioSource.Play();
                 BucketFilled?.Invoke();
             }
-            else if (_fillLevel > maxFillLevel)
+            else if (_fillLevel == maxFillLevel + 1)
             {
-                BucketOverfilled?.Invoke();
+                _bucketAudioSource.clip = transformFailedClip;
+                _bucketAudioSource.Play();
+                Invoke("PlayBucketOverfilled", transformSuccessClip.length);
             }
+        }
+
+        void PlayBucketOverfilled()
+        {
+            BucketOverfilled?.Invoke();
         }
     }
 }

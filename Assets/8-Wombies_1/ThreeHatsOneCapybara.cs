6eab97818f20c8b8a24ecd63b29769de487207ea
Eithan Austria <80587780+ShaEithan@users.noreply.github.com>
2023-01-13 16:42:31 -0600

    Wombies Mini Micro Game Jam PR: NoCap & ZombieBara (#16)
    * commit so far with game elements. capybara on carpet, snakes, background moving.
    
    * Revert "commit so far with game elements. capybara on carpet, snakes, background moving."
    
    This reverts commit 4cdddf5206f8da1bfa2a079287535b9f9e3dff1e.
    
    * Revert "Revert "commit so far with game elements. capybara on carpet, snakes, background moving.""
    
    This reverts commit 9ae2ec065aa1faf255a966766b1ccc0ebc94dbaa.
    
    * Accidentally commited my game to base, but undid changes. Everything is good now
    
    * fixed instantiated gameobjects leaking into main scene
    
    * MVP Code
    
    Added Scripts for Hats and Random Winningness yuh
    
    * implemented art
    
    * Update WIP
    
    * renamed folders, set up 2nd microgame
    
    * deleted extra scene
    
    * Fixed Scripts for New Hats
    
    * game2 counter script and whitebox
    
    * Final Changes to Minigame 1
    
    * Change Title of Game
    
    * edited minigamedefinitions
    
    * created animator controller
    
    * implemented art and animations
    
    * zombie animation play at 15 hits
    
    * Add Sounds
    
    * Added Hat Music
    
    Yuh
    
    * implemented sound game2
    
    * Updated Minigame Def
    
    * edited g2 minigamedef
    
    * fixed audio code
    
    * minor code fix
    
    * fixed background g1
    
    * Added Screenshots to MiniGame Def
    
    * Changed Minigame Definition
    
    * "You win omg"
    
    * Removing .DS Store
    
    Co-authored-by: grizzledwarvet23 <72232563+grizzledwarvet23@users.noreply.github.com>
    Co-authored-by: Matthew Zhang <matthew.zhang.306@gmail.com>
    Co-authored-by: enair3 <91293037+enair3@users.noreply.github.com>

---
+++ Assets/8-Wombies_1/ThreeHatsOneCapybara.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Wombies {
+    public class ThreeHatsOneCapybara : MonoBehaviour {
+
+
+        // Text
+        public TextMeshProUGUI uiText;
+        public string startText;
+        public string winText;
+
+        // Sound
+        public AudioClip winSound;
+        public AudioClip loopSound;
+
+        public int musicTimer = 5;
+
+        // positions must be between 0 and 4 inclusive
+        private int currentPos = 3;
+        private int winningPos;
+
+        private void Start()
+        {
+
+            // audio source loop
+            AudioSource musicLoop = Managers.AudioManager.CreateAudioSource();
+
+            musicLoop.loop = true;
+            musicLoop.clip = loopSound;
+            musicLoop.Play();
+            
+            // randomly assigning 
+            winningPos = UnityEngine.Random.Range(0, 5);
+
+            // start text
+            uiText.text = startText;
+
+        }
+
+        private void Update()
+        {
+            // keep track of current position
+            if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && currentPos < 4)
+            {
+                // move along x axis by how ever much
+                // in our case 4 units (right)
+                currentPos++;
+            }
+
+            if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && currentPos > 0)
+            {
+                currentPos--;
+            }
+
+            // space bar interaction
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+            
+                // reveal what's underneath the hat
+                if (this.gameObject.transform.GetChild(currentPos).GetComponent<Renderer>().enabled)
+                {
+                    this.gameObject.transform.GetChild(currentPos).GetComponent<Renderer>().enabled = false;
+                }
+
+                // if we pick the correct one (win) 
+                if (currentPos == winningPos)
+                {
+                    // stop music loop
+                    this.enabled = false;
+
+                    // render capybara for winning hat
+                    this.gameObject.transform.GetChild(currentPos).GetChild(0).GetComponent<Renderer>().enabled = true;
+
+                    // play sound
+                    AudioSource win = Managers.AudioManager.CreateAudioSource();
+                    win.PlayOneShot(winSound);
+                    
+                    Managers.MinigamesManager.DeclareCurrentMinigameWon();
+                    Managers.MinigamesManager.EndCurrentMinigame(1f);
+                }
+            }
+        }
+    }
+}

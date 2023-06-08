2d60862c54706cb5fa970f59194f8f5453056f51
Adhan Razzaque <adhan.razzaque@gmail.com>
2023-06-07 20:13:18 -0500

    Initial Drawing and Selection mechanic

--- Assets/Final-Boss/Scripts/Card.cs
+++ Assets/Final-Boss/Scripts/Card.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Final_Boss
 {
@@ -21,5 +22,26 @@ namespace Final_Boss
         public int manaCost;
         public int cardValue;
         public int turnsApplied = 1;
+
+        // Events
+        public static event Action<int> CardPlayed; 
+        
+        private bool _hasBeenPlayed;
+        private int _handIndex;
+
+        public void DealCard(int handIndex)
+        {
+            _handIndex = handIndex;
+        }
+
+        private void OnMouseDown()
+        {
+            if (_hasBeenPlayed) return;
+            
+            Debug.Log("Playing card");
+            transform.position += Vector3.up * 5;
+            _hasBeenPlayed = true;
+            CardPlayed?.Invoke(_handIndex);
+        }
     }
 }
--- Assets/Final-Boss/Scripts/GameManager.cs
+++ Assets/Final-Boss/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
@@ -10,23 +11,35 @@ namespace Final_Boss
     {
         public List<Card> deck = new List<Card>();
         public Transform[] cardSlots;
-        
+        public int roundLengthInSeconds = 30;
+
         public TMP_Text deckSizeText;
-        
+        public TMP_Text roundTimerText;
+
         private bool[] _availableCardSlots;
+        private int _currentPlayerHealth;
+        private int _currentEnemyHealth;
+        private Coroutine _roundTimer;
 
         private void Start()
         {
-            _availableCardSlots = new bool[deck.Count];
+            _availableCardSlots = new bool[cardSlots.Length];
             for (var i = 0; i < _availableCardSlots.Length; ++i)
             {
                 _availableCardSlots[i] = true;
             }
-        }
 
-        private void Update()
-        {
             deckSizeText.text = deck.Count.ToString();
+
+            Card.CardPlayed += OnCardPlayed;
+
+            foreach (var card in deck)
+            {
+                card.gameObject.SetActive(false);
+            }
+            
+            // TODO: REMOVE
+            _roundTimer = StartCoroutine(RunRoundTimer());
         }
 
         public void DrawCard()
@@ -40,13 +53,51 @@ namespace Final_Boss
 
             for (var i = 0; i < _availableCardSlots.Length; ++i)
             {
-                if (_availableCardSlots[i] != true) continue;
+                if (_availableCardSlots[i] == false) continue;
+
                 randomCard.gameObject.SetActive(true);
                 randomCard.transform.position = cardSlots[i].position;
+                randomCard.DealCard(i);
                 _availableCardSlots[i] = false;
                 deck.Remove(randomCard);
+                deckSizeText.text = deck.Count.ToString();
                 return;
             }
+            
+            
+            Debug.Log("No available card slots");
+        }
+
+        private void OnCardPlayed(int handIndex)
+        {
+            Debug.Log($"Card {handIndex} was played");
+            _availableCardSlots[handIndex] = true;
+        }
+
+        public void OnConfirmRoundPressed()
+        {
+            if (_roundTimer == null) return;
+
+            var roundTimer = _roundTimer;
+            _roundTimer = null;
+            StopCoroutine(roundTimer);
+            roundTimerText.gameObject.SetActive(false);
+        }
+
+        private IEnumerator RunRoundTimer()
+        {
+            var timeLeft = roundLengthInSeconds;
+            roundTimerText.gameObject.SetActive(true);
+
+            while (timeLeft > 0)
+            {
+                roundTimerText.text = timeLeft.ToString();
+                timeLeft -= 1;
+                yield return new WaitForSeconds(1);
+            }
+
+            roundTimerText.text = "0";
+            roundTimerText.gameObject.SetActive(false);
         }
     }
 }
